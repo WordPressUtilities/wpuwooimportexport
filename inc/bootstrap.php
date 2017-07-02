c37bfdd830302186d807a234dd09543d2e69f042
Kevin <kevin@rocher-dev.com>
2017-07-02 23:36:15 +0200

    v 0.7.0
    * Orders v 0.2.0 : Get Orders.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -2,7 +2,7 @@
 
 /*
 Name: WPU Woo Import/Export
-Version: 0.6.0
+Version: 0.7.0
 Description: A CLI utility to import/export orders & products in WooCommerce
 Author: Darklg
 Author URI: http://darklg.me/
--- inc/orders.php
+++ inc/orders.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-* ORDERS V 0.1.1
+* ORDERS V 0.2.0
 */
 
 include dirname(__FILE__) . '/bootstrap.php';
@@ -9,6 +9,106 @@ include dirname(__FILE__) . '/bootstrap.php';
 class WPUWooImportExport_Orders extends WPUWooImportExport {
     public function __construct() {
         parent::__construct();
+    }
+
+    public function get_orders($datas = array()) {
+        if (!is_array($datas)) {
+            $datas = array();
+        }
+        if (!isset($datas['post_type'])) {
+            $datas['post_type'] = 'shop_order';
+        }
+        if (!isset($datas['posts_per_page'])) {
+            $datas['posts_per_page'] = '20';
+        }
+        if (!isset($datas['orderby'])) {
+            $datas['orderby'] = 'ID';
+        }
+        if (!isset($datas['order'])) {
+            $datas['order'] = 'DESC';
+        }
+        if (!isset($datas['post_status'])) {
+            $datas['post_status'] = 'wc-processing';
+        }
+
+        $list_products = false;
+        if (isset($datas['list_products'])) {
+            $list_products = true;
+            unset($datas['list_products']);
+        }
+
+        $load_billing_address = false;
+        if (isset($datas['load_billing_address'])) {
+            $load_billing_address = true;
+            unset($datas['load_billing_address']);
+        }
+
+        $load_shipping_address = false;
+        if (isset($datas['load_shipping_address'])) {
+            $load_shipping_address = true;
+            unset($datas['load_shipping_address']);
+        }
+
+        $order_posts = get_posts($datas);
+        $orders = array();
+        foreach ($order_posts as $order_post) {
+
+            $wc_order = new WC_Order($order_post->ID);
+
+            $order = array(
+                'id' => $order_post->ID,
+                'date' => $order_post->post_date,
+                'total' => $wc_order->get_total()
+            );
+
+            if ($load_billing_address) {
+                $order['billing_first_name'] = $wc_order->get_billing_first_name();
+                $order['billing_last_name'] = $wc_order->get_billing_last_name();
+                $order['billing_company'] = $wc_order->get_billing_company();
+                $order['billing_address_1'] = $wc_order->get_billing_address_1();
+                $order['billing_address_2'] = $wc_order->get_billing_address_2();
+                $order['billing_city'] = $wc_order->get_billing_city();
+                $order['billing_state'] = $wc_order->get_billing_state();
+                $order['billing_postcode'] = $wc_order->get_billing_postcode();
+                $order['billing_country'] = $wc_order->get_billing_country();
+            }
+
+            if ($load_shipping_address) {
+                $order['shipping_first_name'] = $wc_order->get_shipping_first_name();
+                $order['shipping_last_name'] = $wc_order->get_shipping_last_name();
+                $order['shipping_company'] = $wc_order->get_shipping_company();
+                $order['shipping_address_1'] = $wc_order->get_shipping_address_1();
+                $order['shipping_address_2'] = $wc_order->get_shipping_address_2();
+                $order['shipping_city'] = $wc_order->get_shipping_city();
+                $order['shipping_state'] = $wc_order->get_shipping_state();
+                $order['shipping_postcode'] = $wc_order->get_shipping_postcode();
+                $order['shipping_country'] = $wc_order->get_shipping_country();
+            }
+
+            $tmp_orders = array();
+
+            if (!$list_products) {
+                $tmp_orders[] = $order;
+            } else {
+                $items = $wc_order->get_items();
+                foreach ($items as $item) {
+                    $order['line_name'] = $item->get_name();
+                    $order['line_total'] = $item->get_total();
+                    $order['line_total_tax'] = $item->get_total_tax();
+                    $tmp_orders[] = $order;
+                }
+            }
+
+            foreach ($tmp_orders as $tmp_order) {
+                if (apply_filters('wpuwooimportexport_orders_use_order', true, $tmp_order)) {
+                    $orders[] = $tmp_order;
+                }
+            }
+
+        }
+
+        return $orders;
+
     }
 
     /* Update orders
