383fa899a3b5e4ee8537895600d2190d9696c2c5
Kevin <kevin@rocher-dev.com>
2019-06-26 19:48:54 +0200

    v 0.27.1.1
    * Return post id when updating a post.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -2,7 +2,7 @@
 
 /*
 Name: WPU Woo Import/Export
-Version: 0.27.1
+Version: 0.27.1.1
 Description: A CLI utility to import/export orders & products in WooCommerce
 Author: Darklg
 Author URI: http://darklg.me/
@@ -259,6 +259,8 @@ class WPUWooImportExport {
             $post_keys['ID'] = $post_id;
             wp_update_post($post_keys);
         }
+
+        return $post_id;
     }
 
     /* Update user from datas
